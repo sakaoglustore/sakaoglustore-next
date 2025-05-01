10dcca08203f299123f9c90ade37c949a5a5ed28
sakaoglustore <murat.sakaoglu.store@gmail.com>
2025-05-01 22:43:47 +0300

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import Navbar from '@/components/Navbar';
 import ChatBot from '@/components/ChatBot'; // ⬅️ ekledik
 
 export default function App({ Component, pageProps }) {
-  return (
+  retur (
     <>
       <Navbar />
       <Component {...pageProps} />
