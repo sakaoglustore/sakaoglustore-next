8f141c93d9fdec81dfc24d3fd120a9b99a704154
ertugrulEgeEyiler <101422654+ertugrulEgeEyiler@users.noreply.github.com>
2025-05-19 15:52:11 +0300

    Update signup.jsx

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -14,9 +14,11 @@ export default function Signup() {
   const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
 
   const handleSubmit = async e => {
-    e.preventDefault();    const allFieldsFilled = Object.values(form).every(field => field.trim() !== '');
+    e.preventDefault();
+    const allFieldsFilled = Object.values(form).every(field => field.trim() !== '');
     if (!allFieldsFilled) return setMessage('Lütfen tüm alanları doldurun.');
-      const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{6,16}$/;
+    
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z]).{6,16}$/;
     if (!passwordRegex.test(form.password)) {
       return setMessage('Şifre geçersiz: 1 büyük, 1 küçük harf ve 6-16 karakter olmalı.');
     }
@@ -31,19 +33,10 @@ export default function Signup() {
     setMessage(data.message);
 
     if (res.status === 201) {
-      const loginRes = await fetch('https://api.sakaoglustore.net/api/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ identifier: form.email, password: form.password })
-      });
-
-      const loginData = await loginRes.json();
-      if (loginRes.status === 200) {
-        localStorage.setItem('user', JSON.stringify(loginData.user));
-        router.push('/');
-      } else {
-        setMessage(loginData.message);
-      }
+      // Başarılı kayıt sonrası login sayfasına yönlendir
+      setTimeout(() => {
+        router.push('/login');
+      }, 2000);
     }
   };
 
